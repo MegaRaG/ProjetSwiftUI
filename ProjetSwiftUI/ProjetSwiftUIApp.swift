ec1e5d2e7895d9fb7add571c69e9720d0c1daab9
Ahmed-Omer <Ahmed.Omerovic@etu.univ-savoie.fr>
2024-01-19 10:33:47 +0100

    Initial Commit

---
+++ ProjetSwiftUI/ProjetSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetSwiftUIApp.swift
+//  ProjetSwiftUI
+//
+//  Created by Ahmed OMEROVIC on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjetSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
