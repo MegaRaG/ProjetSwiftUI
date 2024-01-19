f81e7096500283d737a7a532b986195893d1285d
Ahmed-Omer <Ahmed.Omerovic@etu.univ-savoie.fr>
2024-01-19 11:54:53 +0100

    premCours

--- ProjetSwiftUI/AddItemView.swift
+++ ProjetSwiftUI/AddItemView.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct AddItemView: View {
+    @Environment(\.dismiss) private var dismiss
+    @State var name: String = ""
+    @State var rarity: Rarity = .common
+    @EnvironmentObject var inventory : Inventory
+
+    enum Rarity: String, CaseIterable {
+        case common, uncommon, rare, epic, legendary, unique
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section {
+                TextField("Nom de l'objet", text: $name)
+                Picker("Rareté", selection: $rarity) {
+                    ForEach(Rarity.allCases, id: \.self) { rarity in
+                        Text(rarity.rawValue.capitalized)
+                    }
+                }
+            }
+            Section {
+                Button("Ajouter") {
+                    inventory.addItem(item: name)
+                    dismiss()
+                }
+            }
+        }
     }
 }
+
 
 #Preview {
     AddItemView()
 }
+
--- ProjetSwiftUI/ContentView.swift
+++ ProjetSwiftUI/ContentView.swift
@@ -7,18 +7,51 @@
 
 import SwiftUI
 
+class Inventory: ObservableObject {
+    @Published var loot = ["Epée", "Bouclier", "Armure"]
+    
+    func addItem(item: String) {
+        loot.append(item)
+    }
+}
+
 struct ContentView: View {
+    @ObservedObject var inventory = Inventory()
+    
+    @State var showAddItemView = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                Button(action: {
+                    inventory.addItem(item: "Magie de feu")
+                }, label: {
+                    Text("Ajouter")
+                })
+                ForEach(inventory.loot, id: \.self) { item in
+                    Text(item)
+                }
+            }
+            
+            .sheet(isPresented: $showAddItemView, content: {
+                    AddItemView()
+                    .environmentObject(inventory)
+                })
+            .navigationBarTitle("Loot") // Notre titre de page, choisissez le titre que vous voulez
+                .toolbar(content: { // La barre d'outil de notre page
+                    ToolbarItem(placement: ToolbarItemPlacement.automatic) {
+                        Button(action: {
+                            showAddItemView.toggle() // L'action de notre bouton
+                        }, label: {
+                            Image(systemName: "plus.circle.fill")
+                        })
+                    }
+                })
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
