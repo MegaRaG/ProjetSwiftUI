5a09e52ca2770af0ce8843ee86d6689003e03df5
Ahmed-Omer <Ahmed.Omerovic@etu.univ-savoie.fr>
2024-01-23 12:00:44 +0100

    Avancée

--- ProjetSwiftUI/ContentView.swift
+++ ProjetSwiftUI/ContentView.swift
@@ -12,7 +12,8 @@ class Inventory: ObservableObject {
     @Published var loot: [LootItem] = [
         LootItem(name: "Epée", type: .dagger, rarity: .common, attackStrength: 10, game: Game.emptyGame),
         LootItem(name: "Bouclier", type: .shield, rarity: .uncommon, attackStrength: 5, game: Game.emptyGame),
-        LootItem(name: "Poison", type: .poison, rarity: .unique, attackStrength: nil, game: Game(name: "Elden Ring", genre: .rpg, coverName: "Image"))
+        LootItem(name: "Poison", type: .poison, rarity: .unique, attackStrength: nil, game: Game(name: "Elden Ring", genre: .rpg, coverName: "Image")),
+        LootItem(name: "Dingue", type: .thunder, rarity: .legendary, attackStrength: nil, game: Game(name: "LoL", genre: .rpg, coverName: nil))
     ]
     
     func addItem(item: LootItem) {
@@ -23,7 +24,7 @@ class Inventory: ObservableObject {
 struct ContentView: View {
     @ObservedObject var inventory = Inventory()
     @State var showAddItemView = false
-
+    
     var body: some View {
         NavigationView {
             List {
@@ -34,14 +35,53 @@ struct ContentView: View {
                         }
                     }
                 }
-
-                Section(header: Text("Statistiques")) {
-                    Chart(inventory.loot) { set in
-                        BarMark(x: .value("Année", set.name),
-                                y: .value("Occurence", set.attackStrength ?? 0)
-                                            )
-                                        }.frame(height: 300)
+                
+                Section(header: Text("Statistiques").textCase(nil)) {
+                    VStack(alignment: .center, spacing: 10) {
+                        Text("Vos armes les plus puissantes")
+                            .font(.headline)
+                        Chart(inventory.loot) { set in
+                            BarMark(x: .value("Nom de l'objet", set.name),
+                                    y: .value("Force d'attaque", set.attackStrength ?? 0)
+                            )
+                        }
+                        .frame(height: 300)
+                    }
                 }
+                Section(header: Text("Vos Jeux").textCase(nil)) {
+                    ScrollView(.horizontal) {
+                        HStack(spacing: 10) {
+                            ForEach(inventory.loot, id: \.self) { item in
+                                HStack {
+                                    if let coverImage = item.game.coverName {
+                                        Image(coverImage)
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(width: 50, height: 58)
+                                            .cornerRadius(4)
+                                    } else {
+                                        ZStack {
+                                            RoundedRectangle(cornerRadius: 4)
+                                                .fill(Color.gray)
+                                                .opacity(0.4)
+                                                .frame(width: 50, height: 58)
+                                            Image(systemName: "rectangle.slash")
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fit)
+                                                .frame(width: 30, height: 35)
+                                                .cornerRadius(4)
+                                                .foregroundColor(.black)
+                                                .opacity(0.4)
+                                        }
+                                    }
+                                    Text(item.game.name)
+                                        .padding(.leading, 8)
+                                }
+                            }
+                        }
+                    }
+                }
+                
             }
             .sheet(isPresented: $showAddItemView) {
                 AddItemView()
